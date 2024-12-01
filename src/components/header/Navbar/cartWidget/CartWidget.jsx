ce22cac9065c4386e552f7a492cbe6c9d9536097
Emmendieta <emmendieta12@gmail.com>
2024-12-01 19:03:11 -0300

    creaTuLanding1+Mendieta

--- src/components/header/Navbar/cartWidget/CartWidget.jsx
+++ src/components/header/Navbar/cartWidget/CartWidget.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 
 function CartWidget() {
-    const eventoOnClick = () => alert("Carrito de compras en construcción!");
     return (
-        <div id="divCartWidget" onClick={eventoOnClick}>
-            <i className="fa-solid fa-cart-shopping fa-2xl" style={{color: "white",}} id="hdNbCart"></i>
-        </div>
+        <div id="divCartWidget" style={{ position: "relative" }}>
+        <i className="fa-solid fa-cart-shopping fa-2xl" style={{ color: "white" }} id="hdNbCart"></i>
+        <span style={{
+            position: "absolute",
+            top: "0",
+            right: "0",
+            backgroundColor: "red",
+            borderRadius: "50%",
+            color: "white",
+            padding: "0.2em 0.6em",
+            fontSize: "0.75em",
+            fontWeight: "bold",
+            zIndex: "1"
+        }}>
+            0 {/* Este número puede ser dinámico en el futuro */}
+        </span>
+    </div>
     );
 }
 
